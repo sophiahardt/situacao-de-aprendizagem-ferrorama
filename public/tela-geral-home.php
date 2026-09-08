7375ad80bbdd123992bdf96a52ec7e8434371225
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-09-08 15:43:23 -0300

    add - adicionei redirecionamento para a página de visualização de sensores no menu

--- public/tela-geral-home.php
+++ public/tela-geral-home.php
@@ -25,7 +25,7 @@
                         <a class="nav-link" href="#">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Sensores</a>
+                        <a class="nav-link" href="#" onclick="window.location.href='sensor/visualizar-sensor.php'">Sensores</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Trens</a>
