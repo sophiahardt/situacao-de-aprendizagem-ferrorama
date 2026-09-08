7744c38f4801c0df0e0389689e94c009d3b163d5
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-09-08 20:53:30 -0300

    add - implementei a funcionalidade de exclusão de sensores com redirecionamento após a operação

---
+++ public/sensor/excluir-sensor.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "../../infra/conexao.php";
+
+$id_sensor = $_GET["id"] ?? "";
+
+if ($id_sensor == "") {
+    header("Location: visualizar-sensor.php");
+    exit;
+}
+
+$sql = "DELETE FROM sensor WHERE id_sensor = ?";
+
+$stmt = $conexao->prepare($sql);
+
+$stmt->bind_param("i", $id_sensor);
+
+if ($stmt->execute()) {
+    header("Location: visualizar-sensor.php");
+    exit;
+} else {
+    echo "Erro ao excluir o sensor.";
+}
+
+?>
