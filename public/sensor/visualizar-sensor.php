81c903609b1160193c4e318f21e4d4806a68007b
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-09-10 19:31:48 -0300

    refactor - ajuste na formatação do código HTML e correção do título da página de visualização de sensores

--- public/sensor/visualizar-sensor.php
+++ public/sensor/visualizar-sensor.php
@@ -20,29 +20,17 @@ $resultado = $conexao->query($sql);
 
     <meta charset="UTF-8">
 
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Lista sensores cadastrados</title>
+    <title>Visualização de Sensores</title>
 
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <script
-        type="module"
-        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js">
-    </script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 
-    <script
-        nomodule
-        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js">
-    </script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
-    <link
-        rel="stylesheet"
-        href="../../style/style.css">
+    <link rel="stylesheet" href="../../style/style.css">
 
 </head>
 
@@ -52,116 +40,62 @@ $resultado = $conexao->query($sql);
 
         <div class="container-fluid">
 
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 
                 <span class="navbar-toggler-icon"></span>
 
             </button>
 
-
-            <div
-                class="collapse navbar-collapse"
-                id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarNav">
 
                 <ul class="navbar-nav me-auto">
 
                     <li class="nav-item">
-
-                        <a
-                            class="nav-link"
-                            href="../tela-geral-home.php">
-
+                        <a class="nav-link" href="../tela-geral-home.php">
                             Dashboard
-
                         </a>
-
                     </li>
 
-
                     <li class="nav-item">
-
-                        <a
-                            class="nav-link"
-                            href="visualizar-sensor.php">
-
+                        <a class="nav-link" href="../sensor/visualizar-sensor.php">
                             Sensores
-
                         </a>
-
                     </li>
 
-
                     <li class="nav-item">
-
-                        <a
-                            class="nav-link"
-                            href="#">
-
+                        <a class="nav-link" href="../trem/visualizar-trem.php">
                             Trens
-
                         </a>
-
                     </li>
 
-
                     <li class="nav-item">
-
-                        <a
-                            class="nav-link"
-                            href="#">
-
+                        <a class="nav-link" href="../rota/visualizar-rota.php">
                             Rotas
-
                         </a>
-
                     </li>
 
-
                     <li class="nav-item">
-
-                        <a
-                            class="nav-link"
-                            href="#">
-
+                        <a class="nav-link" href="../funcionario/visualizar-funcionario.php">
                             Funcionários
-
                         </a>
-
                     </li>
 
-
                     <li class="nav-item">
-
-                        <a
-                            class="nav-link"
-                            href="#">
-
+                        <a class="nav-link" href="../relatorio/visualizar-relatorio.php">
                             Relatórios
-
                         </a>
-
                     </li>
 
                 </ul>
-
 
                 <ul class="navbar-nav ms-auto align-items-center">
 
                     <li class="nav-item me-3">
 
-                        <span
-                            class="nav-link d-flex align-items-center gap-2">
+                        <span class="nav-link d-flex align-items-center gap-2">
 
-                            <ion-icon
-                                name="person-circle-outline">
-                            </ion-icon>
+                            <ion-icon name="person-circle-outline"></ion-icon>
 
                             João
 
@@ -169,22 +103,13 @@ $resultado = $conexao->query($sql);
 
                     </li>
 
-
                     <li class="nav-item">
 
-                        <a
-                            class="btn btn-outline-light btn-sm d-flex align-items-center gap-2"
-                            href="#">
+                        <a class="btn btn-outline-light btn-sm d-flex align-items-center gap-2" href="#">
 
-                            <ion-icon
-                                name="log-out-outline">
-                            </ion-icon>
+                            <ion-icon name="log-out-outline"></ion-icon>
 
-                            <span>
-
-                                Sair
-
-                            </span>
+                            <span>Sair</span>
 
                         </a>
 
@@ -201,18 +126,13 @@ $resultado = $conexao->query($sql);
 
     <div class="container-fluid p-4">
 
-        <div
-            class="d-flex justify-content-between align-items-center mb-4">
+        <div class="d-flex justify-content-between align-items-center mb-4">
 
             <h1 class="h3">
-
                 Lista de sensores cadastrados
-
             </h1>
 
-
-            <button
-                class="btn btn-primary"
+            <button class="btn btn-primary"
                 style="background-color: #003399;"
                 onclick="window.location.href='cadastrar-sensor.php'">
 
@@ -259,18 +179,12 @@ $resultado = $conexao->query($sql);
                                 <tr>
 
                                     <td>
-
                                         <?= $sensor["id_sensor"] ?>
-
                                     </td>
-
 
                                     <td>
-
                                         <?= $sensor["nome_sensor"] ?>
-
                                     </td>
-
 
                                     <td>
 
@@ -280,7 +194,7 @@ $resultado = $conexao->query($sql);
 
                                             echo $sensor["nome_rota"];
 
-                                        } elseif ($sensor["localizacao"] == "trem") {
+                                        } else {
 
                                             echo $sensor["nome_trem"];
 
@@ -290,51 +204,15 @@ $resultado = $conexao->query($sql);
 
                                     </td>
 
-
                                     <td>
-
-                                        <?php
-
-                                        if ($sensor["tipo_sensor"] == "Velocidade") {
-
-                                            echo '<ion-icon name="speedometer"></ion-icon>';
-
-                                        }
-
-                                        if ($sensor["tipo_sensor"] == "Temperatura") {
-
-                                            echo '<ion-icon name="thermometer"></ion-icon>';
-
-                                        }
-
-                                        if ($sensor["tipo_sensor"] == "Presença") {
-
-                                            echo '<ion-icon name="person"></ion-icon>';
-
-                                        }
-
-                                        ?>
 
                                         <?= $sensor["tipo_sensor"] ?>
 
                                     </td>
 
-
                                     <td>
 
-                                        <a
-                                            href="visualizar-sensor.php?id=<?= $sensor["id_sensor"] ?>"
-                                            class="btn btn-info btn-sm text-white">
-
-                                            <ion-icon name="eye"></ion-icon>
-
-                                            Visualizar
-
-                                        </a>
-
-
-                                        <a
-                                            href="editar-sensor.php?id=<?= $sensor["id_sensor"] ?>"
+                                        <a href="editar-sensor.php?id=<?= $sensor["id_sensor"] ?>"
                                             class="btn btn-warning btn-sm">
 
                                             <ion-icon name="pencil"></ion-icon>
@@ -344,8 +222,7 @@ $resultado = $conexao->query($sql);
                                         </a>
 
 
-                                        <a
-                                            href="excluir-sensor.php?id=<?= $sensor["id_sensor"] ?>"
+                                        <a href="excluir-sensor.php?id=<?= $sensor["id_sensor"] ?>"
                                             class="btn btn-danger btn-sm"
                                             onclick="return confirm('Tem certeza que deseja excluir este sensor?');">
 
@@ -374,9 +251,7 @@ $resultado = $conexao->query($sql);
     </div>
 
 
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
