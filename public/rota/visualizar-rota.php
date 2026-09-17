766c60aecc5a28ba86d2aad96ecaa146091e8421
Brayan <brayan_anzini@estudante.sesisenai.org.br>
2026-09-17 11:09:54 -0300

    fix - add estrutura HTML visualizar-rota.php

--- public/rota/visualizar-rota.php
+++ public/rota/visualizar-rota.php
@@ -11,3 +11,26 @@ $resultado = $conexao->query($sql);
 $rotas = $resultado ? $resultado->fetch_all(MYSQLI_ASSOC) : [];
 
 ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Rotas Cadastradas</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <script type="module"
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js">
+    </script>
+
+    <script nomodule
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js">
+    </script>
+
+    <link rel="stylesheet" href="../../style/style.css">
+
+</head>
