7b8d68cd30b9041e50787bc51ad1ff1991072638
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-09-17 09:54:44 -0300

    fix - ajustei o redirecionamento e títulos na tela excluir-user.php

--- public/user/excluir-user.php
+++ public/user/excluir-user.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Excluir Funcionário</title>
+<title>Excluir Usuário</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="../style.css">
@@ -67,7 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link active" href="visualizar-user.php">Funcionários</a>
+                        <a class="nav-link active" href="visualizar-user.php">Usuários</a>
                     </li>
 
                     <li class="nav-item">
